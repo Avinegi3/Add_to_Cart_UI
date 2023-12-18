0c8a4560518c832246739cb5f47a9b4f308ba64c
Avi Negi <68230390+Avinegi3@users.noreply.github.com>
2023-12-18 21:30:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function changeSize(){
+    let allBoxes = document.querySelectorAll(".boxes") ;
+    let lastActiveBox ;
+    if(document.getElementById("defaultActiveBox")){
+        lastActiveBox = document.getElementById("defaultActiveBox") ;
+        let radioBtn = lastActiveBox.querySelector(".radioBtn") ;
+        radioBtn.classList.add("isClicked"); 
+    }
+    allBoxes.forEach((box) => {
+         box.addEventListener("click" , () => {
+            if(!!lastActiveBox){
+                lastActiveBox.classList.remove("active");
+                let radioBtn = lastActiveBox.querySelector(".radioBtn") ;
+                radioBtn.classList.remove("isClicked");
+            }
+            box.classList.add("active") ;
+            let radioBtn = box.querySelector(".radioBtn") ;
+            radioBtn.classList.add("isClicked");
+            lastActiveBox = box ;
+         })
+    })
+}
+
+changeSize();
